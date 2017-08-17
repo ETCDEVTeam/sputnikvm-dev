c199386e1edfdb6fc9fdd6c8f6e39f24f355dff6
Wei Tang <hi@that.world>
2017-08-17 13:39:39 +0800

    Fix a typo in exposing state interface

--- src/miner/mod.rs
+++ src/miner/mod.rs
@@ -18,9 +18,9 @@ use blockchain::chain::HeaderHash;
 
 mod state;
 
-pub use state::{append_pending_transaction,
-                block_height, get_block_by_hash, get_block_by_number, current_block,
-                get_transaction_by_hash, trie_database};
+pub use self::state::{append_pending_transaction,
+                      block_height, get_block_by_hash, get_block_by_number, current_block,
+                      get_transaction_by_hash, trie_database};
 
 fn transit<'a>(
     database: &MemoryDatabase,
