86144d77474f80e535701b8c95d9dd13852167dd
Igor Artamonov <igor@artamonov.ru>
2017-09-04 16:48:11 -0400

    problem: invalid paths on travis

--- .ci/gcloud-deploy.sh
+++ .ci/gcloud-deploy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+set -e
+
 VERSION_BASE=$(janus version -format='v%M.%m.x')
 echo "Deploy to http://builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/"
 
 mkdir deploy
-mv svm-dev* ./deploy/
+mv svmdev* ./deploy/
 
-janus deploy -to="builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/" -files="./deploy/*" -key=".ci/gcloud.json"
+janus deploy -to="builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/" -files="./deploy/*" -key=".ci/.gcloud.json"
 
 echo "Deployed"
