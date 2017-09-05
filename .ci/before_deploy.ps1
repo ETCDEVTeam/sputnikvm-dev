71f9c024284e136cd37ba1b3c81f97ee7c425c2e
Igor Artamonov <igor@artamonov.ru>
2017-09-04 20:57:50 -0400

    problem: invalid path on appveyor

--- .ci/before_deploy.ps1
+++ .ci/before_deploy.ps1
@@ -12,7 +12,7 @@ $ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET
 $ZIP_SHA256 = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET).zip.sha256"
 
 # TODO Update this to package the right artifacts
-Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\svmdev.exe" '$SRC_DIR\svmdev-win-$($Env:VERSION).exe'
+Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\svmdev.exe" "$SRC_DIR\svmdev-win-$($Env:VERSION).exe"
 
 7z a "$ZIP" *
 Get-FileHash "$ZIP" -Algorithm SHA256 | Out-File "$ZIP_SHA256"
