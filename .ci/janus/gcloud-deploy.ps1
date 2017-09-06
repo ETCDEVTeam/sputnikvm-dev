73682b5f9caa9ae186fb47a1b7f8cc648ad9baf6
Wei Tang <hi@that.world>
2017-09-06 23:48:35 +0800

    [WIP] Appveyor: quotation problem

--- .ci/janus/gcloud-deploy.ps1
+++ .ci/janus/gcloud-deploy.ps1
@@ -1,6 +1,6 @@
-echo "Rust version is "$($env:RUST_VERSION)"
+echo "Rust version is $($env:RUST_VERSION)"
 
-If ($env:APPVEYOR_REPO_BRANCH -eq "gcp-deploy" -AND $env:RUST_VERSION -eq "stable'") {
+If ($env:APPVEYOR_REPO_BRANCH -eq "gcp-deploy" -AND $env:RUST_VERSION -eq "stable") {
     echo "Deploy to http://builds.etcdevteam.com/sputnikvm-dev/$env:VERSION_BASE/"
     janus.exe deploy -to="builds.etcdevteam.com/sputnikvm-dev/$env:VERSION_BASE/" -files=".\janus\*.zip" -key=".\.ci\.gcloud.json"
 }
