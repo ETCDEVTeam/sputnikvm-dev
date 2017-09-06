b83fcd4246553051eab07b8761d2191032be19ca
Wei Tang <hi@that.world>
2017-09-06 23:21:43 +0800

    Appveyor: only deploy GCP when rust version is stable

--- .ci/janus/gcloud-deploy.ps1
+++ .ci/janus/gcloud-deploy.ps1
@@ -1,4 +1,4 @@
-If ($env:APPVEYOR_REPO_BRANCH -eq 'gcp-deploy') {
+If ($env:APPVEYOR_REPO_BRANCH -eq 'gcp-deploy' -And $env:RUST_VERSION -eq 'stable') {
     echo "Deploy to http://builds.etcdevteam.com/sputnikvm-dev/$env:VERSION_BASE/"
     janus.exe deploy -to="builds.etcdevteam.com/sputnikvm-dev/$env:VERSION_BASE/" -files=".\janus\*.zip" -key=".\.ci\.gcloud.json"
 }
