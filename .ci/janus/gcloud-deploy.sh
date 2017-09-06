321f17743c95e8d7809a99c886a25169d85c51fb
Wei Tang <hi@that.world>
2017-09-06 19:50:00 +0800

    [WIP] Move Travis janus build to test phrase

--- .ci/janus/gcloud-deploy.sh
+++ .ci/janus/gcloud-deploy.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-VERSION_BASE=$(janus version -format='v%M.%m.x')
-echo "Deploy to http://builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/"
+if [ "$TRAVIS_BRANCH" == "gce-deploy" ] && [ "$TRAVIS_RUST_VERSION" == "stable" ]; then
+    VERSION_BASE=$(janus version -format='v%M.%m.x')
+    echo "Deploy to http://builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/"
 
-janus deploy -to="builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/" -files="./janus/*" -key=".ci/.gcloud.json"
+    janus deploy -to="builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/" -files="./janus/*" -key=".ci/.gcloud.json"
 
-echo "Deployed"
+    echo "Deployed"
+fi
