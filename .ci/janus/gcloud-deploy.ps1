f4dca27638f9782510f319ecc5f7fbb83d21bba5
Wei Tang <hi@that.world>
2017-09-06 23:43:51 +0800

    [WIP] Try fixing the version issue

--- .ci/janus/gcloud-deploy.ps1
+++ .ci/janus/gcloud-deploy.ps1
@@ -1,4 +1,6 @@
-If ($env:APPVEYOR_REPO_BRANCH -eq 'gcp-deploy' -And $env:RUST_VERSION -eq 'stable') {
+echo "Rust version is "$($env:RUST_VERSION)"
+
+If ($env:APPVEYOR_REPO_BRANCH -eq "gcp-deploy" -AND $env:RUST_VERSION -eq "stable'") {
     echo "Deploy to http://builds.etcdevteam.com/sputnikvm-dev/$env:VERSION_BASE/"
     janus.exe deploy -to="builds.etcdevteam.com/sputnikvm-dev/$env:VERSION_BASE/" -files=".\janus\*.zip" -key=".\.ci\.gcloud.json"
 }
