46695f38fcd5b8c48cdee70baecf0fbacc5a7400
Wei Tang <hi@that.world>
2017-09-07 23:19:20 +0800

    [WIP] Travis: use nested if as workaround

--- .ci/janus/gcloud-deploy.sh
+++ .ci/janus/gcloud-deploy.sh
@@ -2,11 +2,13 @@
 
 set -e
 
-if [[ ("$TRAVIS_BRANCH" == "gce-deploy") && ("$TRAVIS_RUST_VERSION" == "stable") ]]; then
-    VERSION_BASE=$(janus version -format='v%M.%m.x')
-    echo "Deploy to http://builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/"
+if [ "$TRAVIS_BRANCH" -eq "gce-deploy" ]; then
+    if [ "$TRAVIS_RUST_VERSION" -eq "stable" ]; then
+        VERSION_BASE=$(janus version -format='v%M.%m.x')
+        echo "Deploy to http://builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/"
 
-    janus deploy -to="builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/" -files="./janus/*" -key=".ci/.gcloud.json"
+        janus deploy -to="builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/" -files="./janus/*" -key=".ci/.gcloud.json"
 
-    echo "Deployed"
+        echo "Deployed"
+    fi
 fi
