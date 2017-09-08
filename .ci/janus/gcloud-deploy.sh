a3891abf52ce67f9d10ef8bcd2a47af6b5b993f5
Wei Tang <hi@that.world>
2017-09-08 22:59:28 +0800

    Travis: only deploy when it's not a pull request

--- .ci/janus/gcloud-deploy.sh
+++ .ci/janus/gcloud-deploy.sh
@@ -5,13 +5,15 @@ set -e
 export PATH=$PATH:$(pwd)/janusbin
 echo "Check condition branch: $TRAVIS_BRANCH rust version: $TRAVIS_RUST_VERSION"
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    if [ "$TRAVIS_RUST_VERSION" = "stable" ]; then
-        VERSION_BASE=$(janus version -format='v%M.%m.x')
-        echo "Deploy to http://builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/"
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    if [ "$TRAVIS_BRANCH" = "master" ]; then
+        if [ "$TRAVIS_RUST_VERSION" = "stable" ]; then
+            VERSION_BASE=$(janus version -format='v%M.%m.x')
+            echo "Deploy to http://builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/"
 
-        janus deploy -to="builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/" -files="./janus/*" -key=".ci/.gcloud.json"
+            janus deploy -to="builds.etcdevteam.com/sputnikvm-dev/$VERSION_BASE/" -files="./janus/*" -key=".ci/.gcloud.json"
 
-        echo "Deployed"
+            echo "Deployed"
+        fi
     fi
 fi
