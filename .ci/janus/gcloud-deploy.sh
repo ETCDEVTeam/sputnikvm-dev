312e3b91b5473980645b756edc9ca46ee804c29b
Wei Tang <hi@that.world>
2017-09-08 15:01:00 +0800

    [WIP] Travis: print debug message

--- .ci/janus/gcloud-deploy.sh
+++ .ci/janus/gcloud-deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "Check condition branch: $TRAVIS_BRANCH rust version: $TRAVIS_RUST_VERSION"
+
 if [ "$TRAVIS_BRANCH" = "gce-deploy" ]; then
     if [ "$TRAVIS_RUST_VERSION" = "stable" ]; then
         VERSION_BASE=$(janus version -format='v%M.%m.x')
