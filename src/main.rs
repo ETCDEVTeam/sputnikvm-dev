55bed9276ec6f1376db782a40a1720356739b586
Wei Tang <hi@that.world>
2018-02-13 20:50:45 +0800

    Multi-chain support
    With this, one can run `svmdev` with `--chain <network>` or `--chain
    <network>-<fork-name>` (eg. `--chain classic-homestead`) to use a
    particular network config. The supported network list is the same as
    the SputnikVM supported network list.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate sputnikvm;
 extern crate sputnikvm_stateful;
-extern crate sputnikvm_network_classic;
 extern crate secp256k1;
 extern crate rand;
 extern crate sha3;
@@ -27,6 +26,13 @@ extern crate clap;
 extern crate log;
 extern crate env_logger;
 
+extern crate sputnikvm_network_classic;
+extern crate sputnikvm_network_foundation;
+extern crate sputnikvm_network_ubiq;
+extern crate sputnikvm_network_ellaism;
+extern crate sputnikvm_network_expanse;
+extern crate sputnikvm_network_musicoin;
+
 #[cfg(feature = "frontend")]
 extern crate hyper;
 
@@ -47,11 +53,44 @@ use std::thread;
 use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{channel, Sender, Receiver};
-use sputnikvm_network_classic::MainnetEIP160Patch;
+use sputnikvm::Patch;
+
+use sputnikvm_network_classic::{
+    MainnetEIP160Patch as PClassicEIP160,
+    MainnetEIP150Patch as PClassicEIP150,
+    MainnetFrontierPatch as PClassicFrontier,
+    MainnetHomesteadPatch as PClassicHomestead,
+    MordenEIP160Patch as PMordenEIP160,
+    MordenEIP150Patch as PMordenEIP150,
+    MordenFrontierPatch as PMordenFrontier,
+    MordenHomesteadPatch as PMordenHomestead,
+};
+use sputnikvm_network_foundation::{
+    FrontierPatch as PFoundationFrontier,
+    HomesteadPatch as PFoundationHomestead,
+    EIP150Patch as PFoundationEIP150,
+    SpuriousDragonPatch as PFoundationSpuriousDragon,
+    ByzantiumPatch as PFoundationByzantium,
+};
+use sputnikvm_network_ellaism::{
+    MainnetEIP160Patch as PEllaismEIP160,
+};
+use sputnikvm_network_expanse::{
+    FrontierPatch as PExpanseFrontier,
+    HomesteadPatch as PExpanseHomestead,
+    SpuriousDragonPatch as PExpanseSpuriousDragon,
+    ByzantiumPatch as PExpanseByzantium,
+};
+use sputnikvm_network_musicoin::{
+    MainnetFrontierPatch as PMusicoinFrontier,
+    MainnetHomesteadPatch as PMusicoinHomestead,
+};
+use sputnikvm_network_ubiq::{
+    SpuriousDragonPatch as PUbiqSpuriousDragon,
+};
 
 fn main() {
     env_logger::init();
-    let mut rng = OsRng::new().unwrap();
 
     let matches = clap_app!(
         svmdev =>
@@ -62,7 +101,53 @@ fn main() {
             (@arg BALANCE: -b --balance +takes_value "Balance in Wei for the account to be generated, default is 0x10000000000000000000000000000.")
             (@arg LISTEN: -l --listen +takes_value "Listen address and port for the RPC, e.g. 127.0.0.1:8545.")
             (@arg ACCOUNTS: -a --accounts +takes_value "Additional accounts to be generated, default to 9.")
+            (@arg CHAIN: -c --chain +takes_value "Specify the chain to use. Refer to the documentation for a full list of valid values.")
     ).get_matches();
+
+    match matches.value_of("CHAIN") {
+        None => with_patch::<PClassicEIP160>(matches),
+
+        Some("classic") => with_patch::<PClassicEIP160>(matches),
+        Some("classic-eip160") => with_patch::<PClassicEIP160>(matches),
+        Some("classic-eip150") => with_patch::<PClassicEIP150>(matches),
+        Some("classic-homestead") => with_patch::<PClassicHomestead>(matches),
+        Some("classic-frontier") => with_patch::<PClassicFrontier>(matches),
+
+        Some("morden") => with_patch::<PMordenEIP160>(matches),
+        Some("morden-eip160") => with_patch::<PMordenEIP160>(matches),
+        Some("morden-eip150") => with_patch::<PMordenEIP150>(matches),
+        Some("morden-homestead") => with_patch::<PMordenHomestead>(matches),
+        Some("morden-frontier") => with_patch::<PMordenFrontier>(matches),
+
+        Some("foundation") => with_patch::<PFoundationByzantium>(matches),
+        Some("foundation-byzantium") => with_patch::<PFoundationByzantium>(matches),
+        Some("foundation-spurious-dragon") => with_patch::<PFoundationSpuriousDragon>(matches),
+        Some("foundation-eip150") => with_patch::<PFoundationEIP150>(matches),
+        Some("foundation-homestead") => with_patch::<PFoundationHomestead>(matches),
+        Some("foundation-frontier") => with_patch::<PFoundationFrontier>(matches),
+
+        Some("ellaism") => with_patch::<PEllaismEIP160>(matches),
+        Some("ellaism-eip160") => with_patch::<PEllaismEIP160>(matches),
+
+        Some("expanse") => with_patch::<PExpanseByzantium>(matches),
+        Some("expanse-byzantium") => with_patch::<PExpanseByzantium>(matches),
+        Some("expanse-spurious-dragon") => with_patch::<PExpanseSpuriousDragon>(matches),
+        Some("expanse-homestead") => with_patch::<PExpanseHomestead>(matches),
+        Some("expanse-frontier") => with_patch::<PExpanseFrontier>(matches),
+
+        Some("musicoin") => with_patch::<PMusicoinHomestead>(matches),
+        Some("musicoin-homestead") => with_patch::<PMusicoinHomestead>(matches),
+        Some("musicoin-frontier") => with_patch::<PMusicoinFrontier>(matches),
+
+        Some("ubiq") => with_patch::<PUbiqSpuriousDragon>(matches),
+        Some("ubiq-spurious-dragon") => with_patch::<PUbiqSpuriousDragon>(matches),
+
+        _ => panic!("Unsupported chain."),
+    }
+}
+
+fn with_patch<'a, P: 'static + Patch + Send>(matches: clap::ArgMatches<'a>) {
+    let mut rng = OsRng::new().unwrap();
 
     let secret_key = match matches.value_of("PRIVATE_KEY") {
         Some(val) => SecretKey::from_slice(&SECP256K1, &read_hex(val).unwrap()).unwrap(),
@@ -91,13 +176,13 @@ fn main() {
 
     let (sender, receiver) = channel::<bool>();
 
-    let state = miner::make_state::<MainnetEIP160Patch>(genesis);
+    let state = miner::make_state::<P>(genesis);
 
     let miner_arc = Arc::new(Mutex::new(state));
     let rpc_arc = miner_arc.clone();
 
     thread::spawn(move || {
-        miner::mine_loop::<MainnetEIP160Patch>(miner_arc, receiver);
+        miner::mine_loop::<P>(miner_arc, receiver);
     });
 
     #[cfg(feature = "frontend")]
@@ -135,7 +220,7 @@ fn main() {
         });
     }
 
-    rpc::rpc_loop::<MainnetEIP160Patch>(
+    rpc::rpc_loop::<P>(
         rpc_arc,
         &matches.value_of("LISTEN").unwrap_or("127.0.0.1:8545").parse().unwrap(),
         sender);
